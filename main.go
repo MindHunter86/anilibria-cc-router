cee755fb3deef37c98464264a6e13e9ecf1abfaf
Vadimka Komissarov <admin@vkom.cc>
2022-12-30 04:02:35 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/urfave/cli/v2"
+)
+
+var version = "devel" // -ldflags="-X 'main.version=X.X.X'"
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6068", nil))
+	}()
+
+	// logger
+	log := zerolog.New(zerolog.ConsoleWriter{
+		Out: os.Stderr,
+	}).With().Timestamp().Logger().Hook(SeverityHook{})
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	// application
+	app := cli.NewApp()
+	cli.VersionFlag = &cli.BoolFlag{Name: "version", Aliases: []string{"V"}}
+
+	app.Name = "anilibria-cc-router"
+	app.Version = version
+	app.Compiled = time.Now()
+	app.Authors = []*cli.Author{
+		&cli.Author{
+			Name:  "MindHunter86",
+			Email: "mindhunter86@vkom.cc",
+		},
+	}
+	app.Copyright = "(c) 2022-2023 mindhunter86\nwith love for Anilibria project"
+	app.Usage = ""
+
+	app.Flags = []cli.Flag{
+		// common flags
+		&cli.StringFlag{
+			Name:    "log-level",
+			Aliases: []string{"l"},
+			Value:   "debug",
+			Usage:   "levels: trace, debug, info, warn, err, panic, disabled",
+			EnvVars: []string{"LOG_LEVEL"},
+		},
+		&cli.BoolFlag{
+			Name:    "quite",
+			Aliases: []string{"q"},
+			Usage:   "Flag is equivalent to --log-level=quite",
+		},
+	}
+
+	app.Action = func(c *cli.Context) (e error) {
+		var lvl zerolog.Level
+		if lvl, e = zerolog.ParseLevel(c.String("log-level")); e != nil {
+			log.Fatal().Err(e)
+		}
+
+		zerolog.SetGlobalLevel(lvl)
+		if c.Bool("quite") {
+			zerolog.SetGlobalLevel(zerolog.Disabled)
+		}
+
+		log.Debug().Msg("ready...")
+		log.Debug().Strs("args", os.Args).Msg("")
+
+		return application.NewApp(c, &log).Bootstrap()
+	}
+
+	// TODO sort.Sort of Flags uses too much allocs; temporary disabled
+	// sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	if e := app.Run(os.Args); e != nil {
+		log.Fatal().Err(e).Msg("")
+	}
+}
+
+type SeverityHook struct{}
+
+func (SeverityHook) Run(e *zerolog.Event, level zerolog.Level, _ string) {
+	if level > zerolog.DebugLevel || version != "devel" {
+		return
+	}
+
+	rfn := "unknown"
+	pcs := make([]uintptr, 1)
+
+	if runtime.Callers(4, pcs) != 0 {
+		if fun := runtime.FuncForPC(pcs[0] - 1); fun != nil {
+			rfn = fun.Name()
+		}
+	}
+
+	fn := strings.Split(rfn, "/")
+	e.Str("func", fn[len(fn)-1:][0])
+}
